99b3a221a2438faa1428630564d6b93fdb9aa417
Nelo-T. Wallus <nelo@wallus.de>
2020-02-18 22:05:57 +0100

    Support running a daemonized imapfilter

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,15 +11,22 @@ else
     config_target="$IMAPFILTER_CONFIG"
 fi
 
+updated_config=no
 pull_config() {
+    updated_config=no
     if [ ! -d "$config_target_prefix" ]; then
         printf ">>> Config has not been cloned yet, cloning\n"
         mkdir -p "$config_target_prefix"
+        updated_config=yes
         git clone "https://$GIT_USER:$(cat $GIT_TOKEN)@$GIT_TARGET" "$config_target_prefix"
     else
         cd "$config_target_prefix"
         printf ">>> Pulling config\n"
-        git pull
+        git remote update
+        if [ "$(git rev-parse HEAD)" != "$(git rev-parse FETCH_HEAD)" ]; then
+            updated_config=yes
+            git pull
+        fi
         cd -
     fi
 }
@@ -38,6 +45,20 @@ if [ ! -f "$config_target" ]; then
     exit 1
 fi
 
+imapfilter_pid=
+imapfilter_update() {
+    if [ -n "$imapfilter_pid" ]; then
+        kill -TERM "$imapfilter_pid"
+        wait "$imapfilter_pid"
+    fi
+    imapfilter -c "$config_target" &
+    imapfilter_pid="$(jobs -p)"
+}
+
+if [ "$IMAPFILTER_DAEMON" = "yes" ]; then
+    imapfilter_update
+fi
+
 while true; do
     if config_in_vcs; then
         printf ">>> Updating config\n"
@@ -46,8 +67,15 @@ while true; do
         fi
     fi
 
-    printf ">>> Running imapfilter\n"
-    imapfilter -c "$config_target"
+    if [ "$IMAPFILTER_DAEMON" = "yes" ]; then
+        if [ "$updated_config" = "yes" ]; then
+            printf ">>> Restarting imapfilter daemon\n"
+            imapfilter_update
+        fi
+    else
+        printf ">>> Running imapfilter\n"
+        imapfilter -c "$config_target"
+    fi
 
     printf ">>> Sleeping\n"
     sleep "${IMAPFILTER_SLEEP:-30}"

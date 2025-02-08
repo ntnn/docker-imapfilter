2a031bce9b1cbb2fca39697f1109d360ef3c19d0
Nelo-T. Wallus <nelo@wallus.de>
2025-02-08 12:46:37 +0100

    Restructure entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,7 +30,6 @@ config_in_vcs() {
     [ -n "$(vcs_token)" ] && [ -n "$GIT_TARGET" ]
 }
 
-
 config_target_prefix="/opt/imapfilter/config"
 if config_in_vcs; then
     config_target="$config_target_prefix/$IMAPFILTER_CONFIG"
@@ -38,78 +37,92 @@ else
     config_target="$IMAPFILTER_CONFIG"
 fi
 
-log_parameter=
-if [ -n "$IMAPFILTER_LOGFILE" ]; then
-	log_parameter="-l $IMAPFILTER_LOGFILE"
-fi
-
-updated_config=no
 pull_config() {
-    updated_config=no
+    config_in_vcs || return
+
+    printf ">>> Updating config\n"
     if [ ! -d "$config_target_prefix" ]; then
         printf ">>> Config has not been cloned yet, cloning\n"
         mkdir -p "$config_target_prefix"
-        updated_config=yes
-
         git clone "$(vcs_uri)" "$config_target_prefix"
+        return
     else
         cd "$config_target_prefix"
         printf ">>> Pulling config\n"
         git remote update
         if [ "$(git rev-parse HEAD)" != "$(git rev-parse FETCH_HEAD)" ]; then
-            updated_config=yes
             git pull
+            return
         fi
         cd -
     fi
+    return 1
 }
 
-if config_in_vcs; then
-    pull_config
-    cd "$config_target_prefix"
-elif [ -n "$IMAPFILTER_CONFIG_BASE" ]; then
-    cd "$IMAPFILTER_CONFIG_BASE"
-else
-    cd "${config_target%/*}"
-fi
+start_imapfilter() {
+    # enter a subshell to not affect the pwd of the running process
+    (
+        # Enter the basedir of the config. Required to allow relative
+        # includes in the lua scripts.
+        if config_in_vcs; then
+            cd "$config_target_prefix"
+        elif [ -n "$IMAPFILTER_CONFIG_BASE" ]; then
+            cd "$IMAPFILTER_CONFIG_BASE"
+        else
+            cd "${config_target%/*}"
+        fi
 
+        log_parameter=
+        if [ -n "$IMAPFILTER_LOGFILE" ]; then
+                log_parameter="-l $IMAPFILTER_LOGFILE"
+        fi
+        imapfilter -c "$config_target" $log_parameter
+    )
+}
+
+imapfilter_pid=
+imapfilter_restart_daemon() {
+    if [ -n "$imapfilter_pid" ]; then
+        kill -TERM "$imapfilter_pid"
+        wait "$imapfilter_pid"
+    fi
+    start_imapfilter &
+    imapfilter_pid="$(jobs -p)"
+}
+
+loop_no_daemon() {
+    while true; do
+        pull_config
+
+        printf ">>> Running imapfilter\n"
+        start_imapfilter
+
+        printf ">>> Sleeping\n"
+        sleep "${IMAPFILTER_SLEEP:-30}"
+    done
+}
+
+loop_daemon() {
+    imapfilter_restart_daemon
+    while true; do
+        if pull_config; then
+            printf ">>> Update in VCS, restarting imapfilter daemon\n"
+            imapfilter_restart_daemon
+        fi
+
+        printf ">>> Sleeping\n"
+        sleep "${IMAPFILTER_SLEEP:-30}"
+    done
+}
+
+pull_config
 if [ ! -f "$config_target" ]; then
     printf "Config file '%s' does not exist\n" "$config_target"
     exit 1
 fi
 
-imapfilter_pid=
-imapfilter_update() {
-    if [ -n "$imapfilter_pid" ]; then
-        kill -TERM "$imapfilter_pid"
-        wait "$imapfilter_pid"
-    fi
-    imapfilter -c "$config_target" $log_parameter &
-    imapfilter_pid="$(jobs -p)"
-}
-
 if [ "$IMAPFILTER_DAEMON" = "yes" ]; then
-    imapfilter_update
+    loop_daemon
+else
+    loop_no_daemon
 fi
-
-while true; do
-    if config_in_vcs; then
-        printf ">>> Updating config\n"
-        if ! pull_config; then
-            printf ">>> Pulling config failed\n"
-        fi
-    fi
-
-    if [ "$IMAPFILTER_DAEMON" = "yes" ]; then
-        if [ "$updated_config" = "yes" ]; then
-            printf ">>> Restarting imapfilter daemon\n"
-            imapfilter_update
-        fi
-    else
-        printf ">>> Running imapfilter\n"
-        imapfilter -c "$config_target" $log_parameter
-    fi
-
-    printf ">>> Sleeping\n"
-    sleep "${IMAPFILTER_SLEEP:-30}"
-done
